b9d4083f95afb7237d44b834795e938661d20650
Dil-sl <88966407+Dil-sl@users.noreply.github.com>
2024-05-12 16:15:37 +0530

    login Backend success

---
+++ QuizApp-API/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizApp_API.Data;
+using QuizApp_API.Models;
+
+namespace QuizApp_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext appDbContext;
+
+        public UserController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> Get() => await appDbContext.Users.ToListAsync();
+        [HttpPost]
+        public async Task<ActionResult<User>> Add(User user)
+        {
+            if (user != null)
+            {
+                var result = appDbContext.Users.Add(user).Entity;
+                await appDbContext.SaveChangesAsync();
+                return Ok(result);
+            }
+            return BadRequest("Invalid Request");
+        }
+
+        [HttpGet("{email}/{password}")]
+        public async Task<ActionResult<User>> Login(string email, string password)
+        {
+            if (email is not null && password is not null)
+            {
+                var user = await appDbContext.Users
+                    .Where(x => x.Email!.ToLower().Equals(email.ToLower()) && x.Password == password)
+                    .FirstOrDefaultAsync();
+                return user != null ? Ok(user) : NotFound("User not found");
+            }
+            return BadRequest("Invalid Request");
+        }
+    }
+}
--- QuizApp-API/Data/AppDbContext.cs
+++ QuizApp-API/Data/AppDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace QuizApp_API.Data
+﻿using Microsoft.EntityFrameworkCore;
+using QuizApp_API.Models;
+
+namespace QuizApp_API.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; } = default!;
     }
 }
